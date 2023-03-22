b23bc49de5b12c5ef05512b0ed46618fda10cc4f
thao-ng11 <thao.ng11@gmail.com>
2023-03-22 09:59:03 -0400

    sky and ground surface imported

--- class_tutorial.py
+++ class_tutorial.py
@@ -2,15 +2,25 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((800,400)) 
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+
 while True:
+     #create event loop to check for player i nput
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            # run the sys module to close any running code
+            #end while loop
+            #must import first
             exit()
+    
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
     # draw all our elements
     # update everything
     pygame.display.update() 
